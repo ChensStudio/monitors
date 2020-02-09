1de9c5f5e321d5a832e38824417ed5130f50bbfa
Yang Chen <young.y.chen@gmail.com>
2020-02-08 18:58:29 -0800

    add email receipts

--- monitorBootNodes.js
+++ monitorBootNodes.js
@@ -62,7 +62,7 @@ function sendMail(mailBody, instruction) {
 
     var mailOptions = {
         from: 'helpdesk@pasnet.us',
-        to: 'yang.chen@moac.io; xinle.yang@moac.io; zhengpeng.li@moac.io; david.chen@moac.io; qing.xu@moac.io; qxu@mossglobal.net',
+        to: 'yang.chen@moac.io; xinle.yang@moac.io; zhengpeng.li@moac.io; david.chen@moac.io; qing.xu@moac.io; qxu@mossglobal.net; 2032834752@qq.com',
         subject: 'Moac Boot Node Issue',
         text: mailBody + '\n' + instruction
     };
