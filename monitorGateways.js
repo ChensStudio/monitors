e550233b5dbbc61045ae316babb9048fdea4dd99
Yang Chen <young.y.chen@gmail.com>
2021-03-08 12:53:33 -0800

    remove north virginia server

--- monitorGateways.js
+++ monitorGateways.js
@@ -13,7 +13,6 @@ let monitorMainnetGatewayUrls = [
     { url: 'http://gateway.moac.io/mainnet', alias: 'Mainnet Gateway' }, 
     { url: 'http://124.71.20.30:8932', alias: 'Mainnet China' }, 
     { url: 'http://35.161.103.244:8932', alias: 'Mainnet OR' }, 
-    { url: 'http://3.80.171.1:8932', alias: 'Mainnet N.VA' }, 
     { url: 'http://13.53.72.84:8932', alias: 'Mainnet Stockholm' }, 
     { url: 'http://3.127.39.74:8932', alias: 'Mainnet Frankfurt' }, 
     { url: 'http://35.178.188.187:8932', alias: 'Mainnet London' }
